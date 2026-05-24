0ec33f332163f31434c626d725a53563f4f0b55f
Jose Manuel Puicon <josemanuelpr23@gmail.com>
2026-05-24 18:12:32 +0000

    Force dynamic rendering on /personal (useSearchParams)

--- app/personal/page.tsx
+++ app/personal/page.tsx
@@ -1,13 +1,12 @@
 import { Layout } from "@/components/Layout";
 import { PersonalModule } from "@/components/PersonalModule";
 
+export const dynamic = "force-dynamic";
+
 export default function PersonalPage() {
   return (
     <Layout
       title="Personal"
       description="Registro de trabajadores, roles, asistencia y pagos semanales."
     >
-      <PersonalModule />
-    </Layout>
-  );
-}
+     
